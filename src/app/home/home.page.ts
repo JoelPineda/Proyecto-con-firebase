74ef75fc3071107da35b63a5f412f15599d8c9cb
Joel Pineda <joelpinedabeltre815@gmail.com>
2020-02-29 21:52:36 -0400

    Proyecto con ionic

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskI } from '../models/task.interface';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage  implements OnInit{
+  todos: TaskI[];
+
+  constructor(private todoService: TodoService){}
+
+  ngOnInit(){
+    this.todoService.getTodos().subscribe((todos) =>{
+      console.log('Todos', todos);
+      this.todos = todos;
+    });
+    
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCJBczw9Vw50clJSN2UbwU5DelGI8npMcU",
+    authDomain: "crud-a6dac.firebaseapp.com",
+    databaseURL: "https://crud-a6dac.firebaseio.com",
+    projectId: "crud-a6dac",
+    storageBucket: "crud-a6dac.appspot.com",
+    messagingSenderId: "976156027267",
+    appId: "1:976156027267:web:8841fda2ae4ab707be6eb6",
+    measurementId: "G-525K1KCJYX"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
